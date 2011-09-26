093169991b842a7f1b9aeaa7b51d7e21ebd57761
Janne Haapsaari <haaja@iki.fi>
2011-09-26 23:14:20 +0300

    Added solution for problem 15.

---
+++ problem15.py
@@ -0,0 +1,20 @@
+"""
+Starting in the top left corner of a 2x2 grid, there are 6 routes (without 
+backtracking) to the bottom right corner.
+
+How many routes are there through a 20x20 grid?
+"""
+
+import math
+
+def solve_problem():
+    
+    rows = 20
+    cols = 20
+
+    #return math.factorial(rows+cols)/(math.factorial(rows)*math.factorial(cols))
+
+    return math.factorial(rows+cols) / (math.factorial(rows) * math.factorial(cols))
+
+if __name__ == '__main__':
+    print(solve_problem())
